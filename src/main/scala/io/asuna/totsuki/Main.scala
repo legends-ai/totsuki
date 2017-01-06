a26bdc19bb70d0c313670a82a3cc0b9d751387fb
Ian Macalinao <me@ian.pw>
2017-01-06 12:47:01 -0600

    one line main

--- src/main/scala/io/asuna/totsuki/Main.scala
+++ src/main/scala/io/asuna/totsuki/Main.scala
@@ -5,8 +5,7 @@ import monix.execution.Scheduler.Implicits.global
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val totsuki = new TotsukiServer(args)
-    totsuki.standReady()
+    new TotsukiServer(args).standReady()
   }
 
 }
