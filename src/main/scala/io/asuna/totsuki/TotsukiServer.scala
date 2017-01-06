6c57f2d804d0a0812768a6bccc6f295bdcdf68da
Ian Macalinao <me@ian.pw>
2017-01-06 13:17:23 -0600

    Use more Observable functionality for added flexibility, and improve outfile naming

--- src/main/scala/io/asuna/totsuki/TotsukiServer.scala
+++ src/main/scala/io/asuna/totsuki/TotsukiServer.scala
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.transfer.TransferManager
 import com.google.protobuf.empty.Empty
 import io.asuna.proto.enums.Region
 import java.io.{ File, FileOutputStream }
+import java.util.UUID
 import monix.execution.Scheduler
 import scala.concurrent.duration._
 import io.asuna.asunasan.BaseService
@@ -21,49 +22,50 @@ class TotsukiServer(args: Seq[String])(implicit scheduler: Scheduler)
 
   val stream = PublishToOneSubject[RawMatch]()
 
-  // Create the RawMatch processor
+  // Create the RawMatch processor.
   stream
-    .bufferTimed(config.service.writeInterval.seconds)
-    .foreach(writeMatches)
 
-  override def write(rawMatch: RawMatch): Future[Empty] = endpoint {
-    for {
-      _ <- stream.onNext(rawMatch)
-    } yield Empty.defaultInstance
-  }
+    // First, we will group matches by region/version.
+    .groupBy { m =>
+      (m.id.map(_.region).getOrElse(Region.UNDEFINED_REGION), m.data.map(_.version).orEmpty)
+    }
 
-  def writeMatches(matches: Seq[RawMatch]): Unit = {
-    matches
-      // First, we will group matches by region/version.
-      .groupBy { m =>
-        (m.id.map(_.region).getOrElse(Region.UNDEFINED_REGION), m.data.map(_.version).orEmpty)
-      }
-
-      // We filter out matches that don't have a region or version.
-      // We aren't using Option here in case we get an undefined region or version object.
-      .filterKeys {
+    // We filter out matches that don't have a region or version.
+    // We aren't using Option here in case we just have a malformed object.
+    .filter { observable =>
+      observable.key match {
         case (region, version) => region != Region.UNDEFINED_REGION && version != ""
       }
+    }
 
-      // Then, we write the matches of each bucket.
-      .foreach {
-        case ((region, version), matches) => writeBucketMatches(region, version, matches)
-      }
+    // Now we iterate over all of the region/version combinations.
+    // TODO(igm): evaluate whether we want to wait potentially longer to store more matches.
+    // We may also want to apply back pressure. See https://monix.io/api/2.1/monix/reactive/Observable.html.
+    .foreach { obs =>
+      val (region, version) = obs.key
+      obs
+        .bufferTimed(config.service.writeInterval.seconds)
+        .foreach(writeBucketMatches(region, version, _))
+    }
+
+  override def write(rawMatch: RawMatch): Future[Empty] = endpoint {
+    stream.onNext(rawMatch).map(_ => Empty.defaultInstance)
   }
 
   /**
-    *  Writes matches of a specific region/version pair.
+    *  Writes matches of a specific region/version combination.
     */
   def writeBucketMatches(region: Region, version: String, matches: Seq[RawMatch]): Unit = {
-    // First, we fetch a timestamp to distinguish this bucket write.
-    val time = System.currentTimeMillis()
+    // First, we generate a unique id to distinguish this bucket write.
+    // We will also provide a timestamp to make it easy to know what the latest write was.
+    val id = s"$${System.currentTimeMillis}_{UUID.randomUUID()}"
 
     val rgName = region.name.toLowerCase()
 
     // Next, we create a temp file to write the matches to.
     // This makes it possible for us to do multipart uploads to S3, rather than streaming
     // directly to S3. There is one temp file per region/version pair per time.
-    val file = File.createTempFile(s"totsuki-${rgName}-${version}-${time}", ".tmp")
+    val file = File.createTempFile(s"totsuki_${rgName}_${version}_${id}", ".tmp")
 
     // Here we write the matches to the tempfile.
     val fs = new FileOutputStream(file)
@@ -73,7 +75,7 @@ class TotsukiServer(args: Seq[String])(implicit scheduler: Scheduler)
     // Here we upload the temp file to S3.
     // TransferManager will automatically make this multipart if it will improve performance.
     tx.upload(config.service.bucket,
-              s"${rgName}/${version}/${time}.protolist", file).waitForCompletion()
+              s"${rgName}/${version}/${id}.protolist", file).waitForCompletion()
 
     // Now we delete the tempfile since the S3 upload is complete.
     file.delete()
